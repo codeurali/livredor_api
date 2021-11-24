45c29ef401a28f4cdbcbb2bedd21a021ca9eb963
unknown <codeurali.tagazali@gmail.com>
2021-11-24 16:01:09 +0100

    remove cors policy

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const filename = "./data/data.json"
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3003
-const cors = require('cors');
-app.use(cors());
 
 app.use(express.json());
 
